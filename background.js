0b1b5f4c35973eea968e12688fb4117bef1df904
MugiD <bekslambek22@gmail.com>
2025-06-06 08:49:35 +0500

    feat: activate blocker on check

--- background.js
+++ background.js
@@ -1,7 +1,15 @@
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    if(tab.url.includes("https://www.instagram.com/reels/") && changeInfo.status == "loading") {
-        chrome.tabs.update(tabId, {url: "https://youtu.be/dQw4w9WgXcQ?si=ZjaxNvFD2F9O6U-2"});
-    }
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  if (
+    tab.url.includes("https://www.instagram.com/reels/") &&
+    changeInfo.status == "loading"
+  ) {
+    chrome.storage.sync.get("blockReels", (data) => {
+        if (data.blockReels == true) {
 
-    console.log(tab.url);
+        chrome.tabs.update(tabId, {
+          url: "https://youtu.be/dQw4w9WgXcQ?si=ZjaxNvFD2F9O6U-2",
+        });
+      }
+    });
+  }
 });
---
+++ popup/popup.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const blocker = document.getElementById("blocker");
+
+  chrome.storage.sync.get("blockReels", (data) => {
+    blocker.checked = data.blockReels === true;
+  });
+
+  blocker.addEventListener("change", () => {
+    chrome.storage.sync.set({ blockReels: blocker.checked });
+  });
+});
