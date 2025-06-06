842144d572cd12e08ec04c404cb13a9701928b4c
MugiD <bekslambek22@gmail.com>
2025-06-06 09:21:16 +0500

    feat: change text on check

--- popup/popup.js
+++ popup/popup.js
@@ -4,9 +4,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
   chrome.storage.sync.get("blockReels", (data) => {
     blocker.checked = data.blockReels === true;
+    note.textContent = blocker.checked ? "great! reels is now blocked" : "reels blocker is turned off";
   });
 
   blocker.addEventListener("change", () => {
     chrome.storage.sync.set({ blockReels: blocker.checked });
+    note.textContent = blocker.checked ? "great! reels is now blocked" : "reels blocker is turned off";
   });
 });
