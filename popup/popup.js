4aab8353eb741bdfd1e41810c2b0da034a9cf8bb
MugiD <bekslambek22@gmail.com>
2025-06-06 09:16:17 +0500

    ui: change elements' composition

--- popup/popup.js
+++ popup/popup.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   const blocker = document.getElementById("blocker");
+  let note = document.getElementById("note");
 
   chrome.storage.sync.get("blockReels", (data) => {
     blocker.checked = data.blockReels === true;
